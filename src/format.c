40d3b7d3860c6c581dfbff6ad4ab9e20b3dbcc80
fiskusk <klapil.filip@seznam.cz>
2019-11-16 18:41:01 +0100

    Upadted all sources

--- src/format.c
+++ src/format.c
@@ -1,23 +1,31 @@
 #include "format.h"
+#include <stdio.h>
 
 extern float fout;
 
-const char * format_number(prefixes prefix, float *output)
+const char *format_number(prefixes prefix, float *output)
 {
-     switch (prefix) {
-         case kilo:
-            *output=(float)(fout/1e3);
-            return "kHz";
-            break;
-        case mega:
-            *output=(float)(fout/1e6);
-            return "MHz";
-            break;
-        case giga:
-            *output=(float)(fout/1e9);
-            return "GHz";
-            break;
-         default:
-            break;
-     }
- }
+    char *str_k = "kHz";
+    char *str_m = "MHz";
+    char *str_g = "GHz";
+    char *str_return = NULL;
+
+    switch (prefix)
+    {
+    case kilo:
+        *output = (float)(fout / 1e3);
+        str_return = str_k;
+        break;
+    case mega:
+        *output = (float)(fout / 1e6);
+        str_return = str_m;
+        break;
+    case giga:
+        *output = (float)(fout / 1e9);
+        str_return = str_g;
+        break;
+    default:
+        break;
+    }
+    return str_return;
+}
