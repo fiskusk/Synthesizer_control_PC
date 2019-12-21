0dd777354c471cfe00107d869b3fae283870e162
Filip Klapil <klapi.filip@seznam.cz>
2019-12-21 21:21:33 +0100

    added register 0,1 controls, fix OUT buttons text,

--- Synthesizer_PC_SW/Synthesizer_PC_control/Form1.Designer.cs
+++ Synthesizer_PC_SW/Synthesizer_PC_control/Form1.Designer.cs
@@ -33,6 +33,10 @@
             this.Out1Button = new System.Windows.Forms.Button();
             this.Out2Button = new System.Windows.Forms.Button();
             this.PloInitButton = new System.Windows.Forms.Button();
+            this.Reg0TextBox = new System.Windows.Forms.TextBox();
+            this.Reg0Label = new System.Windows.Forms.Label();
+            this.Reg1Label = new System.Windows.Forms.Label();
+            this.Reg1TextBox = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // OpenPortButton
@@ -48,54 +52,109 @@
             // 
             // ClosePortButton
             // 
+            this.ClosePortButton.Enabled = false;
             this.ClosePortButton.Location = new System.Drawing.Point(167, 15);
             this.ClosePortButton.Margin = new System.Windows.Forms.Padding(4);
             this.ClosePortButton.Name = "ClosePortButton";
             this.ClosePortButton.Size = new System.Drawing.Size(100, 28);
             this.ClosePortButton.TabIndex = 0;
             this.ClosePortButton.Text = "Close Port";
-            this.ClosePortButton.Enabled = false;
             this.ClosePortButton.UseVisualStyleBackColor = true;
             this.ClosePortButton.Click += new System.EventHandler(this.ClosePortButton_Click);
             // 
             // Out1Button
             // 
+            this.Out1Button.Enabled = false;
             this.Out1Button.Location = new System.Drawing.Point(16, 61);
             this.Out1Button.Name = "Out1Button";
             this.Out1Button.Size = new System.Drawing.Size(100, 28);
             this.Out1Button.TabIndex = 1;
             this.Out1Button.Text = "Out 1 Off";
-            this.Out1Button.Enabled = false;
             this.Out1Button.UseVisualStyleBackColor = true;
             this.Out1Button.Click += new System.EventHandler(this.Out1Button_Click);
             // 
             // Out2Button
             // 
+            this.Out2Button.Enabled = false;
             this.Out2Button.Location = new System.Drawing.Point(167, 61);
             this.Out2Button.Name = "Out2Button";
             this.Out2Button.Size = new System.Drawing.Size(100, 28);
             this.Out2Button.TabIndex = 1;
             this.Out2Button.Text = "Out 2 Off";
-            this.Out2Button.Enabled = false;
             this.Out2Button.UseVisualStyleBackColor = true;
             this.Out2Button.Click += new System.EventHandler(this.Out2Button_Click);
             // 
             // PloInitButton
             // 
+            this.PloInitButton.Enabled = false;
             this.PloInitButton.Location = new System.Drawing.Point(16, 124);
             this.PloInitButton.Name = "PloInitButton";
             this.PloInitButton.Size = new System.Drawing.Size(100, 28);
             this.PloInitButton.TabIndex = 2;
             this.PloInitButton.Text = "PLO Init";
-            this.PloInitButton.Enabled = false;
             this.PloInitButton.UseVisualStyleBackColor = true;
             this.PloInitButton.Click += new System.EventHandler(this.PloInitButton_Click);
+            // 
+            // Reg0TextBox
+            // 
+            this.Reg0TextBox.BackColor = System.Drawing.Color.White;
+            this.Reg0TextBox.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.Reg0TextBox.Enabled = false;
+            this.Reg0TextBox.Location = new System.Drawing.Point(732, 13);
+            this.Reg0TextBox.MaxLength = 8;
+            this.Reg0TextBox.Name = "Reg0TextBox";
+            this.Reg0TextBox.Size = new System.Drawing.Size(100, 22);
+            this.Reg0TextBox.TabIndex = 3;
+            this.Reg0TextBox.Text = "80C90000";
+            this.Reg0TextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.Reg0TextBox.Click += new System.EventHandler(this.Reg0TextBox_Click);
+            this.Reg0TextBox.TextChanged += new System.EventHandler(this.Reg0TextBox_TextChanged);
+            this.Reg0TextBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Reg0TextBox_KeyDown);
+            // 
+            // Reg0Label
+            // 
+            this.Reg0Label.AutoSize = true;
+            this.Reg0Label.Location = new System.Drawing.Point(653, 16);
+            this.Reg0Label.Name = "Reg0Label";
+            this.Reg0Label.Size = new System.Drawing.Size(73, 17);
+            this.Reg0Label.TabIndex = 4;
+            this.Reg0Label.Text = "Register 0";
+            // 
+            // Reg1Label
+            // 
+            this.Reg1Label.AutoSize = true;
+            this.Reg1Label.Location = new System.Drawing.Point(653, 45);
+            this.Reg1Label.Name = "Reg1Label";
+            this.Reg1Label.Size = new System.Drawing.Size(73, 17);
+            this.Reg1Label.TabIndex = 5;
+            this.Reg1Label.Text = "Register 1";
+            // 
+            // Reg1TextBox
+            // 
+            this.Reg1TextBox.BackColor = System.Drawing.Color.White;
+            this.Reg1TextBox.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.Reg1TextBox.Enabled = false;
+            this.Reg1TextBox.Location = new System.Drawing.Point(732, 42);
+            this.Reg1TextBox.MaxLength = 8;
+            this.Reg1TextBox.Name = "Reg1TextBox";
+            this.Reg1TextBox.Size = new System.Drawing.Size(100, 22);
+            this.Reg1TextBox.TabIndex = 6;
+            this.Reg1TextBox.Text = "800103E9";
+            this.Reg1TextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.Reg1TextBox.Click += new System.EventHandler(this.Reg1TextBox_Click);
+            this.Reg1TextBox.TextChanged += new System.EventHandler(this.Reg1TextBox_TextChanged);
+            this.Reg1TextBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Reg1TextBox_KeyDown);
+
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.Reg1TextBox);
+            this.Controls.Add(this.Reg1Label);
+            this.Controls.Add(this.Reg0Label);
+            this.Controls.Add(this.Reg0TextBox);
             this.Controls.Add(this.PloInitButton);
             this.Controls.Add(this.Out2Button);
             this.Controls.Add(this.Out1Button);
@@ -104,7 +163,9 @@
             this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Synthesizer Control Program by OK2FKU";
+            this.Click += new System.EventHandler(this.Form1_Click);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -115,6 +176,10 @@
         private System.Windows.Forms.Button Out1Button;
         private System.Windows.Forms.Button Out2Button;
         private System.Windows.Forms.Button PloInitButton;
+        private System.Windows.Forms.TextBox Reg0TextBox;
+        private System.Windows.Forms.Label Reg0Label;
+        private System.Windows.Forms.Label Reg1Label;
+        private System.Windows.Forms.TextBox Reg1TextBox;
     }
 }
 
--- Synthesizer_PC_SW/Synthesizer_PC_control/Form1.cs
+++ Synthesizer_PC_SW/Synthesizer_PC_control/Form1.cs
@@ -14,6 +14,10 @@ namespace Synthesizer_PC_control
     public partial class Form1 : Form
     {
         static SerialPort _serialPort;
+        static bool Reg0TextBox_was_focused = false;
+        static bool Reg1TextBox_was_focused = false;
+        static string old_reg0 = "80C90000";
+        static string old_reg1 = "800103E9";
 
         public Form1()
         {
@@ -27,6 +31,8 @@ namespace Synthesizer_PC_control
             Out1Button.Enabled = true;
             Out2Button.Enabled = true;
             PloInitButton.Enabled = true;
+            Reg0TextBox.Enabled = true;
+            Reg1TextBox.Enabled = true;
 
             _serialPort = new SerialPort("COM3", 115200);
             _serialPort.Open();
@@ -40,34 +46,37 @@ namespace Synthesizer_PC_control
             Out1Button.Enabled = false;
             Out2Button.Enabled = false;
             PloInitButton.Enabled = false;
+            Reg0TextBox.Enabled = false;
+            Reg1TextBox.Enabled = false;
+
             _serialPort.Close();
         }
 
         private void Out1Button_Click(object sender, EventArgs e)
         {
-            if (Out1Button.Text == "Out 1 Off")
-            {
-                Out1Button.Text = "Out 1 On";
-                _serialPort.WriteLine("out 1 on");
-            }
-            else if (Out1Button.Text == "Out 1 On")
+            if (Out1Button.Text == "Out 1 On")
             {
                 Out1Button.Text = "Out 1 Off";
+                _serialPort.WriteLine("out 1 on");
+            }
+            else if (Out1Button.Text == "Out 1 Off")
+            {
+                Out1Button.Text = "Out 1 On";
                 _serialPort.WriteLine("out 1 off");
             }
         }
 
         private void Out2Button_Click(object sender, EventArgs e)
         {
-            if (Out2Button.Text == "Out 2 Off")
-            {
-                Out2Button.Text = "Out 2 On";
-                _serialPort.WriteLine("out 2 on");
-            }
-            
-            else if (Out2Button.Text == "Out 2 On")
+            if (Out2Button.Text == "Out 2 On")
             {
                 Out2Button.Text = "Out 2 Off";
+                _serialPort.WriteLine("out 2 on");
+            }
+
+            else if (Out2Button.Text == "Out 2 Off")
+            {
+                Out2Button.Text = "Out 2 On";
                 _serialPort.WriteLine("out 2 off");
             }
         }
@@ -75,6 +84,95 @@ namespace Synthesizer_PC_control
         private void PloInitButton_Click(object sender, EventArgs e)
         {
             _serialPort.WriteLine("PLO init");
+        }
+
+        private void Reg0TextBox_Click(object sender, EventArgs e)
+        {
+            if (Reg0TextBox_was_focused == false)
+            {
+                Reg0TextBox.Focus();
+                Reg0TextBox.SelectAll();
+                Reg0TextBox_was_focused = true;
+            }
+        }
+
+        private void Reg0TextBox_TextChanged(object sender, EventArgs e)
+        {
+            string item = Reg0TextBox.Text;
+            int n = 0;
+            if (!int.TryParse(item, System.Globalization.NumberStyles.HexNumber, System.Globalization.NumberFormatInfo.CurrentInfo, out n) &&
+                item != String.Empty)
+            {
+                Reg0TextBox.Text = item.Remove(item.Length - 1, 1);
+                Reg0TextBox.SelectionStart = Reg0TextBox.Text.Length;
+            }
+        }
+
+        private void Reg0TextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                Form1_Click(this, new EventArgs());
+            }
+        }
+
+        private void Reg1TextBox_Click(object sender, EventArgs e)
+        {
+            if (Reg1TextBox_was_focused == false)
+            {
+                Reg1TextBox.Focus();
+                Reg1TextBox.SelectAll();
+                Reg1TextBox_was_focused = true;
+            }
+        }
+
+        private void Reg1TextBox_TextChanged(object sender, EventArgs e)
+        {
+            string item = Reg1TextBox.Text;
+            int n = 0;
+            if (!int.TryParse(item, System.Globalization.NumberStyles.HexNumber, System.Globalization.NumberFormatInfo.CurrentInfo, out n) &&
+                item != String.Empty)
+            {
+                Reg1TextBox.Text = item.Remove(item.Length - 1, 1);
+                Reg1TextBox.SelectionStart = Reg1TextBox.Text.Length;
+            }
+        }
+        private void Reg1TextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                Form1_Click(this, new EventArgs());
+            }
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            Reg0Label.Focus();
+            Reg0TextBox_was_focused = false;
+            Reg1TextBox_was_focused = false;
+            if ((Reg0TextBox.Enabled == true) && (!Reg0TextBox.Text.Equals(old_reg0)))
+            {
+                ChangeReg0();
+            }
+            if ((Reg1TextBox.Enabled == true) && (!Reg1TextBox.Text.Equals(old_reg1)))
+            {
+                ChangeReg1();
+                ChangeReg0();
+            }
+        }
+
+        private void ChangeReg0()
+        {
+            string data = String.Format("plo set_register {0}", Reg0TextBox.Text);
+            old_reg0 = Reg0TextBox.Text;
+            _serialPort.WriteLine(data);
+        }
+
+        private void ChangeReg1()
+        {
+            string data = String.Format("plo set_register {0}", Reg1TextBox.Text);
+            old_reg1 = Reg0TextBox.Text;
+            _serialPort.WriteLine(data);
         }
     }
 }
