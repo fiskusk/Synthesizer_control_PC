38971865cce225d2353fb82a976a2bb41823c732
Unknown <klapil.filip@seznam.cz>
2020-01-22 12:50:46 +0100

    Copy orig Lukas controller file

--- Synthesizer_PC_SW/Synthesizer_PC_control/controller/Controller.cs
+++ Synthesizer_PC_SW/Synthesizer_PC_control/controller/Controller.cs
@@ -1,80 +1,58 @@
-using System;
-using System.Windows;
-using System.Windows.Forms;
+﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.IO;
-using System.IO.Ports;
-using System.Threading;
-using System.Diagnostics;
-using Newtonsoft.Json;
-using System.Globalization;
+
+using Synthesizer_PC_control.Model;
 
 namespace Synthesizer_PC_control
 {
     class Controller
     {
-        public MyRegister[] registers;
+        private readonly Form1 view;
 
-        public MySerialPort serialPort;
-    // NOTE: static SerialPort _serialPort; => Nevím jsetli to nemusí být static
-        
+        public readonly MySerialPort serialPort;
 
-        public Controller(TextBox[] ui_registers, Button ui_openClosed, ComboBox ui_avaliablePorts)
+        public Controller(Form1 view)
         {
-            // Nastaví se data modelu
-            serialPort = new MySerialPort(ui_openClosed, ui_avaliablePorts);
+            this.view = view;
 
-            // Vytvoření registrů
-            // TODO If config file does not exist
-            // TODO pozor na nutných 6 registrů
-            var reg0 = new MyRegister(ui_registers[0].Text, ui_registers[0]);
-            var reg1 = new MyRegister(ui_registers[1].Text, ui_registers[1]);
-            var reg2 = new MyRegister(ui_registers[2].Text, ui_registers[2]);
-            var reg3 = new MyRegister(ui_registers[3].Text, ui_registers[3]);
-            var reg4 = new MyRegister(ui_registers[4].Text, ui_registers[4]);
-            var reg5 = new MyRegister(ui_registers[5].Text, ui_registers[5]);
-
-            registers = new MyRegister[] { reg0, reg1, reg2, reg3, reg4, reg5};
+            serialPort = new MySerialPort(view, view.textBox, view.PortButton, view.AvaibleCOMsComBox);
         }
 
-#region nejakeRozumneJmenoProSekci
+        #region Serial port
 
-        public void ChangeIntFracMode(int selectedIndex)
+        public void SwitchPort()
         {
-            UInt32 reg0 = registers[0].uint32_GetValue();
-            UInt32 reg2 = registers[2].uint32_GetValue();
-            
-            if (selectedIndex == 0)
-            //if (ModeIntFracComboBox.SelectedIndex == 0)
+            if(serialPort.IsPortOpen())
             {
-                reg0 &= ~unchecked((UInt32)(1<<31));
-                reg2 &= ~unchecked((UInt32)(1<<8));
-                IntNNumUpDown.Minimum = 19;
-                IntNNumUpDown.Maximum = 4091;
-
+                serialPort.ClosePort();
+                view.EnableControls(false);
             }
-            else if (selectedIndex == 1)
-            //else if (RF_A_EN_ComboBox.SelectedIndex == 1)
+            else
             {
-                reg0 |= unchecked((UInt32)(1<<31));
-                reg2 |= unchecked((UInt32)(1<<8));
-                IntNNumUpDown.Minimum = 16;
-                IntNNumUpDown.Maximum = 65535;
+                view.EnableControls( OpenPort() );
             }
-            registers[0].SetValue(reg0);
-            registers[2].SetValue(reg2);
         }
-#endregion
 
-#region  Serial Port
+        private bool OpenPort()
+        {
+            // TODO Use AvaibleCOMsComBox.selectedIndex ?
+            bool succes = serialPort.OpenPort(view.AvaibleCOMsComBox.Text);
 
-#endregion
+            if (succes)
+            {
+                view.SaveWorkspaceData();   // FIXME Not OOD
+                view.EnableControls(true);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
 
+        #endregion
     }
 }
