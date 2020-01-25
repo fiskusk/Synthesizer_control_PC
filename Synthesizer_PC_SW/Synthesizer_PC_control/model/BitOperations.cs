2398ac65ccc537870480bb731e54130fc547e40c
Filip Klapil <klapi.filip@seznam.cz>
2020-01-25 20:34:47 +0100

    addNew ChangeNBits in bitOperations

--- Synthesizer_PC_SW/Synthesizer_PC_control/model/BitOperations.cs
+++ Synthesizer_PC_SW/Synthesizer_PC_control/model/BitOperations.cs
@@ -22,14 +22,25 @@ namespace Synthesizer_PC_control
 
         public static UInt32 ChangeNBits(UInt32 changedVal, UInt32 changingVal, int N, int StartingBit)
         {
+            changedVal &= ~(GetMask(N) << StartingBit);     // Resets the appropriate bit positions
+            changedVal += (changingVal <<  StartingBit);    // Writes the input data to the appropriate positions
+            return changedVal;
+        }
+
+        public static UInt32 GetNBits(UInt32 input_value, int N, int StartingBit)
+        {
+            input_value &= (GetMask(N) << StartingBit);
+            return (input_value >> StartingBit);
+        }
+
+        private static UInt32 GetMask(int N)
+        {
             UInt32 mask = 0;
             for (UInt16 i = 0; i < N; i++)
             {
                 mask += (UInt32)(1 << i);
             }
-            changedVal &= ~(mask << StartingBit);
-            changedVal += (changingVal <<  StartingBit);
-            return changedVal;
+            return mask;
         }
     }
 }
