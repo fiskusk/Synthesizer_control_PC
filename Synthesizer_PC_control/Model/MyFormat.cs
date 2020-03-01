446fcd9be58416345dd94448684abdc3368597ec
Filip Klapil <klapi.filip@seznam.cz>
2020-03-01 09:58:16 +0100

    wip format input into directFreqControl

--- Synthesizer_PC_control/Model/MyFormat.cs
+++ Synthesizer_PC_control/Model/MyFormat.cs
@@ -33,21 +33,168 @@ namespace Synthesizer_PC_control
             }
         }
 
+        /*
         public static void CheckIfHasDecimalInput(TextBox sender)
         {
             string item = sender.Text;
             decimal n = 0;
             if ((!decimal.TryParse(item, NumberStyles.AllowDecimalPoint, new NumberFormatInfo() {
-                NumberDecimalSeparator = ".", NumberGroupSeparator = "" },  out n) &&
+                NumberDecimalSeparator = ".", NumberGroupSeparator = " " },  out n) &&
                 !decimal.TryParse(item, NumberStyles.AllowDecimalPoint, new NumberFormatInfo() {
-                NumberDecimalSeparator = ",", NumberGroupSeparator = "" }, out n)) &&
+                NumberDecimalSeparator = ",", NumberGroupSeparator = " " }, out n)) &&
                 item != String.Empty)
             {
-                int position = sender.SelectionStart-1;
-                item = item.Remove(position, 1);
-                sender.Text = item;
-                sender.SelectionStart = position;
+                if (sender.SelectionStart != 0)
+                {
+                    int position = sender.SelectionStart-1;
+                    item = item.Remove(position, 1);
+                    sender.Text = item;
+                    sender.SelectionStart = position;
+                }
             }
+        }
+        */
+        public static void CheckIfHasDecimalInput(TextBox sender)
+        {
+            string item = sender.Text;
+            int position;
+            position = sender.SelectionStart;
+            bool[] commaIsHere = new bool[item.Length];
+            UInt16 commaCounter = 0;
+
+            for (UInt16 i = 0; i < item.Length; i++)
+            {
+                // check if number with . or , decimal separator input. 
+                // Spaces for separates orders are allowed
+                if (!((item[i] >= '0' && item[i] <= '9') || 
+                       item[i] == ' ' || item[i] == '.' || item[i] == ','))
+                {
+                    item = item.Remove(i, 1);
+                    i--;
+                    position--;
+                }
+
+                // now check, if spaces or decimal separators doesn't repeat
+                else if (i == 0)
+                {
+                    if ((item[i] == ' ') ||
+                        (item[i] == '.' && item[i+1] == '.') ||
+                        (item[i] == ',' && item[i+1] == ',') ||
+                        (item[i] == '.' && item[i+1] == ',') ||
+                        (item[i] == ',' && item[i+1] == '.') ||
+                        (item[i] == ' ' && item[i+1] == '.') ||
+                        (item[i] == ' ' && item[i+1] == ','))
+                    {
+                        item = item.Remove(i, 1);
+                        i--;
+                        position--;
+                    }
+                }
+                else if (i == item.Length - 1)
+                {
+                    if ((item[i] == ' ' && item[i-1] == ' ') ||
+                        (item[i] == '.' && item[i-1] == '.') ||
+                        (item[i] == ',' && item[i-1] == ',') ||
+                        (item[i] == '.' && item[i-1] == ',') ||
+                        (item[i] == ',' && item[i-1] == '.') ||
+                        (item[i] == ' ' && item[i-1] == '.') ||
+                        (item[i] == ' ' && item[i-1] == ',') )
+                    {
+                        item = item.Remove(i, 1);
+                        i--;
+                        position--;
+                    }
+                }
+                else if ((item[i] == ' ' && item[i-1] == ' ') ||
+                    (item[i] == ' ' && item[i+1] == ' ') ||
+                    (item[i] == '.' && item[i-1] == '.') ||
+                    (item[i] == '.' && item[i+1] == '.') ||
+                    (item[i] == ',' && item[i-1] == ',') ||
+                    (item[i] == ',' && item[i+1] == ',') ||
+                    (item[i] == '.' && item[i-1] == ',') ||
+                    (item[i] == '.' && item[i+1] == ',') ||
+                    (item[i] == ',' && item[i-1] == '.') ||
+                    (item[i] == ',' && item[i+1] == '.') )
+                {
+                    item = item.Remove(i, 1);
+                    i--;
+                    position--;
+                }
+
+                // here counts how many decimal separators inputs
+                if (i <= item.Length)
+                {
+                    if (item[i] == ',' || item[i] == '.')
+                    {
+                        commaIsHere[i] = true;
+                        commaCounter++;
+                    }
+                    else
+                        commaIsHere[i] = false;
+                }
+            }
+
+            if (commaCounter == 2)
+            {
+                for (int i = 0; i < item.Length; i++)
+                {
+                    // if cursor position refer next decimal separator, delete it
+                    if (i == position-1 && commaIsHere[i] == true)
+                    {
+                        item = item.Remove(i, 1);
+                        commaCounter--;
+                        i--;
+                        position--;
+                    }
+                }
+            }
+            if (commaCounter > 2)
+            {
+                for (int i = item.Length - 1; i >= 0; i--)
+                {
+                    // if cursor position refer next decimal separator, delete it
+                    if (commaIsHere[i] == true)
+                    {
+                        item = item.Remove(i, 1);
+                        for (int x = i; x < item.Length; x++) {
+                            commaIsHere[x] = commaIsHere[x + 1];
+                        }
+                        commaCounter--;
+                        i--;
+                        position--;
+                    }
+                    if (commaCounter == 1)
+                        i = 0;
+                }
+            }
+            
+            item = item.Replace(',', '.');
+            int comma_position = item.IndexOf(".");
+            if (position == comma_position + 4  && comma_position != -1)
+            {
+               item = item.Insert(comma_position + 4, " ");
+               position++;
+            }
+            if (position == comma_position + 9 && comma_position != -1)
+            {
+               item = item.Remove(comma_position + 8, 1);
+               position--;
+            }
+            sender.Text = item;
+            sender.SelectionStart = position;/*
+            item = item.Replace(" ", string.Empty);
+            item = item.Replace(".", ",");
+            item = item.Replace(",,", ",");
+            item = item.Replace("..", ",");
+            item = item.Replace(",.", ",");
+            item = item.Replace(".,", ",");
+            int comma_position = item.IndexOf(",");
+            if (comma_position != position && comma_position != -1)
+                item = item.Remove(position, 1);
+            decimal itemDecimal = Convert.ToDecimal(item);
+            sender.Text =  itemDecimal.ToString("0.0## ###");
+            if (!(position == comma_position + 1) && position == comma_position + 4)
+                position++;*/
         }
 
         public static void CheckIfHasIntegerInput(TextBox sender)
@@ -109,6 +256,7 @@ namespace Synthesizer_PC_control
             
         }
 
+        /*
         public static string ParseFrequencyDecimalValue(decimal freq)
         {
             UInt16 f_MHz = (UInt16)(freq);
@@ -122,6 +270,6 @@ namespace Synthesizer_PC_control
             double roundedBillionths  = Math.Round((float)(billionths)/100.0, MidpointRounding.AwayFromZero);
 
             return string.Format("{0},{1:000} {2:000} {3:0}", f_MHz, thousandths, millionths, roundedBillionths);
-        }
+        }*/
     }
 }
