b8f7e316076e7584fd2e4514dd40044005adeae5
Filip Klapil <klapi.filip@seznam.cz>
2020-04-01 15:25:25 +0200

    added code comments for Memory class

--- Synthesizer_PC_control/Model/Memory.cs
+++ Synthesizer_PC_control/Model/Memory.cs
@@ -4,21 +4,67 @@ using System.Drawing;
 
 namespace Synthesizer_PC_control.Model
 {
+    /// <summary>
+    /// This class is used to handle the synthesizer module memory.
+    /// </summary>
     class Memory : I_UiLinked
     {
+        /// <summary>
+        /// registers for memory 1
+        /// </summary>
         private MyRegister[] registersMemory1;
+
+        /// <summary>
+        /// registers for memory 2
+        /// </summary>
         private MyRegister[] registersMemory2;
+
+        /// <summary>
+        /// registers for memory 3
+        /// </summary>
         private MyRegister[] registersMemory3;
+
+        /// <summary>
+        /// registers for memory 4
+        /// </summary>
         private MyRegister[] registersMemory4;
 
+        /// <summary>
+        /// hold value if memory has active output 1
+        /// </summary>
         private bool[] memActiveOut1;
-        private bool[] memActiveOut2;
-        private bool[] memIntRefState;
-        private decimal[] memFreq1Value;
-        private decimal[] memFreq2Value;
-        private int[] memPwrAIndex;
-        private int[] memPwrBIndex;
 
+        /// <summary>
+        /// hold value if memory has active output 2
+        /// </summary>
+        private bool[] memActiveOut2;
+
+        /// <summary>
+        /// hold value if memory has active internal reference
+        /// </summary>
+        private bool[] memIntRefState;
+
+        /// <summary>
+        /// hold frequency at output 1 for memory
+        /// </summary>
+        private decimal[] memFreq1Value;
+
+        /// <summary>
+        /// hold frequency at output 2 for memory
+        /// </summary>
+        private decimal[] memFreq2Value;
+
+        /// <summary>
+        /// hold power at output 1 for memory
+        /// </summary>
+        private int[] memPwrAIndex;
+
+        /// <summary>
+        /// hold power at output 1 for memory
+        /// </summary>
+        private int[] memPwrBIndex;
+        
+        // hold memory UI elements
         private readonly Label[] ui_memActiveOut1;
         private readonly Label[] ui_memActiveOut2;
         private readonly Label[] ui_memIntRefState;
@@ -27,6 +73,13 @@ namespace Synthesizer_PC_control.Model
         private readonly Label[] ui_memPwrAIndex;
         private readonly Label[] ui_memPwrBIndex;
 
+        /// <summary>
+        /// Constructor for Memory class
+        /// </summary>
+        /// <param name="registersMemory1"> registers for memory 1 as MyRegister[] </param>
+        /// <param name="registersMemory2"> registers for memory 2 as MyRegister[] </param>
+        /// <param name="registersMemory3"> registers for memory 3 as MyRegister[] </param>
+        /// <param name="registersMemory4"> registers for memory 4 as MyRegister[] </param>
         public Memory(MyRegister[] registersMemory1, MyRegister[] registersMemory2,
                       MyRegister[] registersMemory3, MyRegister[] registersMemory4)
         {
@@ -36,15 +89,22 @@ namespace Synthesizer_PC_control.Model
             this.registersMemory4 = registersMemory4;
         }
 
+        /// <summary>
+        /// Constructor for Memory class
+        /// </summary>
+        /// <param name="view"> handle for all Form1 </param>
         public Memory(Form1 view)
-        {
+        {   
+            // memory 1-4 and registers 0-5 is for synthesizer registers,
+            // register 6 is control and his porpose is storing module controls 
+            // (outputs 1/2 state, internal or external reference)
             var mem1Reg0 = new MyRegister(string.Empty, view.R0M1);
             var mem1Reg1 = new MyRegister(string.Empty, view.R1M1);
             var mem1Reg2 = new MyRegister(string.Empty, view.R2M1);
             var mem1Reg3 = new MyRegister(string.Empty, view.R3M1);
             var mem1Reg4 = new MyRegister(string.Empty, view.R4M1);
             var mem1Reg5 = new MyRegister(string.Empty, view.R5M1);
-            var mem1ControlReg = new MyRegister(string.Empty);
+            var mem1ControlReg = new MyRegister(string.Empty);  
 
             registersMemory1 = new MyRegister[] {mem1Reg0, mem1Reg1, mem1Reg2, mem1Reg3, mem1Reg4, mem1Reg5, mem1ControlReg};
 
@@ -124,112 +184,6 @@ namespace Synthesizer_PC_control.Model
             memPwrBIndex = new int[] {0, 0, 3, 0};
         }
 
-        public MyRegister GetRegister(int memory, int regIndex)
-        {
-            if (regIndex < 0 || regIndex > 6)
-                return null;
-            switch (memory)
-            {
-                case 1:
-                    return registersMemory1[regIndex];
-                case 2:
-                    return registersMemory2[regIndex];
-                case 3:
-                    return registersMemory3[regIndex];
-                case 4:
-                    return registersMemory4[regIndex];
-                default:
-                    return null;
-            }
-        }
-
-        public bool GetMemOut1State(int memory)
-        {
-            return memActiveOut1[--memory];
-        }
-
-        public bool GetMemOut2State(int memory)
-        {
-            return memActiveOut2[--memory];
-        }
-
-        public bool GetIntRefState(int memory)
-        {
-            return memIntRefState[--memory];
-        }
-
-        public void SetMemOut1State(bool state, int memory)
-        {
-            if (state != memActiveOut1[--memory])
-            {
-                memActiveOut1[memory] = state;
-
-                UpdateUiElements();
-            }
-        }
-
-        public void SetMemOut2State(bool state, int memory)
-        {
-            if (state != memActiveOut2[--memory])
-            {
-                memActiveOut2[memory] = state;
-
-                UpdateUiElements();
-            }
-        }
-
-        public void SetMemIntRefState(bool state, int memory)
-        {
-            if (state != memIntRefState[--memory])
-            {
-                memIntRefState[memory] = state;
-
-                UpdateUiElements();
-            }
-        }
-
-        public void SetMemFreq1Value(decimal value, UInt16 memoryNumber)
-        {
-            if (value != memFreq1Value[--memoryNumber])
-            {
-                memFreq1Value[memoryNumber] = value;
-
-                UpdateUiElements();
-            }
-
-        }
-        public void SetMemFreq2Value(decimal value, UInt16 memoryNumber)
-        {
-            if (value != memFreq2Value[--memoryNumber])
-            {
-                memFreq2Value[memoryNumber] = value;
-
-                UpdateUiElements();
-            }
-
-        }
-        public void SetMemPwrAIndex(int value, UInt16 memoryNumber)
-        {
-            if (value != memPwrAIndex[--memoryNumber])
-            {
-                memPwrAIndex[memoryNumber] = value;
-
-                UpdateUiElements();
-            }
-
-        }
-        public void SetMemPwrBIndex(int value, UInt16 memoryNumber)
-        {
-            if (value != memPwrBIndex[--memoryNumber])
-            {
-                memPwrBIndex[memoryNumber] = value;
-
-                UpdateUiElements();
-            }
-
-        }
-        
-
         /// <summary>
         /// Call this to refresh all registers in memory.
         /// </summary>
@@ -239,14 +193,17 @@ namespace Synthesizer_PC_control.Model
             {
                 register.UpdateUiElements();
             }
+
             foreach (var register in registersMemory2)
             {
                 register.UpdateUiElements();
             }
+
             foreach (var register in registersMemory3)
             {
                 register.UpdateUiElements();
             }
+
             foreach (var register in registersMemory4)
             {
                 register.UpdateUiElements();
@@ -340,5 +297,189 @@ namespace Synthesizer_PC_control.Model
                 }
             }
         }
+
+        #region Getters
+
+        /// <summary>
+        /// Call this function to get specific memory synthesizer register
+        /// </summary>
+        /// <param name="memory"> memory number (1-4) </param>
+        /// <param name="regIndex"> specific register index (0-5) </param>
+        /// <returns> 'null' if is bad sellected memory or register index, otherwise MyRegister </returns>
+        public MyRegister GetRegister(int memory, int regIndex)
+        {
+            if (regIndex < 0 || regIndex > 6)
+                return null;
+
+            switch (memory)
+            {
+                case 1:
+                    return registersMemory1[regIndex];
+                case 2:
+                    return registersMemory2[regIndex];
+                case 3:
+                    return registersMemory3[regIndex];
+                case 4:
+                    return registersMemory4[regIndex];
+                default:
+                    return null;
+            }
+        }
+
+        /// <summary>
+        /// This function return if specific memory have active output 1
+        /// </summary>
+        /// <param name="memory"> memory number (1-4) </param>
+        /// <returns> true if enabled, false if disabled </returns>
+        public bool GetMemOut1State(int memory)
+        {
+            return memActiveOut1[--memory];
+        }
+        
+        /// <summary>
+        /// This function return if specific memory have active output 2
+        /// </summary>
+        /// <param name="memory"> memory number (1-4) </param>
+        /// <returns> true if enabled, false if disabled </returns>
+        public bool GetMemOut2State(int memory)
+        {
+            return memActiveOut2[--memory];
+        }
+
+        /// <summary>
+        /// This function return if specific memory have active internal reference or external 
+        /// </summary>
+        /// <param name="memory"> memory number (1-4) </param>
+        /// <returns> true - internal reference, false - external reference </returns>
+        public bool GetIntRefState(int memory)
+        {
+            return memIntRefState[--memory];
+        }
+
+        #endregion
+
+        #region Setters
+
+        /// <summary>
+        /// This function set state of active output 1 for specific memory 
+        /// </summary>
+        /// <param name="state"> true if enabled, false if disabled </param>
+        /// <param name="memory"> specific memory number (1-4) </param>
+        public void SetMemOut1State(bool state, int memory)
+        {
+            if (state != memActiveOut1[--memory])
+            {
+                memActiveOut1[memory] = state;
+
+                UpdateUiElements();
+            }
+        }
+
+        /// <summary>
+        /// This function set state of active output 2 for specific memory 
+        /// </summary>
+        /// <param name="state"> true if enabled, false if disabled </param>
+        /// <param name="memory"> specific memory number (1-4) </param>
+        public void SetMemOut2State(bool state, int memory)
+        {
+            if (state != memActiveOut2[--memory])
+            {
+                memActiveOut2[memory] = state;
+
+                UpdateUiElements();
+            }
+        }
+
+        /// <summary>
+        /// This function set internal reference state for specific memory 
+        /// </summary>
+        /// <param name="state"> true if internal, false if external </param>
+        /// <param name="memory"> specific memory number (1-4) </param>
+        public void SetMemIntRefState(bool state, int memory)
+        {
+            if (state != memIntRefState[--memory])
+            {
+                memIntRefState[memory] = state;
+
+                UpdateUiElements();
+            }
+        }
+
+        /// <summary>
+        /// This function set frequency at output 1 for specific memory 
+        /// </summary>
+        /// <param name="value"> decimal value of frequency </param>
+        /// <param name="memoryNumber"> specific memory number (1-4) </param>
+        public void SetMemFreq1Value(decimal value, UInt16 memoryNumber)
+        {
+            if (value != memFreq1Value[--memoryNumber])
+            {
+                memFreq1Value[memoryNumber] = value;
+
+                UpdateUiElements();
+            }
+
+        }
+
+        /// <summary>
+        /// This function set frequency at output 1 for specific memory 
+        /// </summary>
+        /// <param name="value"> decimal value of frequency </param>
+        /// <param name="memoryNumber"> specific memory number (1-4) </param>
+        public void SetMemFreq2Value(decimal value, UInt16 memoryNumber)
+        {
+            if (value != memFreq2Value[--memoryNumber])
+            {
+                memFreq2Value[memoryNumber] = value;
+
+                UpdateUiElements();
+            }
+
+        }
+
+        /// <summary>
+        /// This function set index of power at synthesizer output A for specific memory 
+        /// </summary>
+        /// <param name="value"> index of power 
+        /// ('0' for "-4dBm", 
+        ///  '1' for "-1dBm", 
+        ///  '2' for "+2dBm", 
+        ///  '3' for "+5dBm", 
+        ///  otherwise "Disabled" )
+        /// </param>
+        /// <param name="memoryNumber"> specific memory number (1-4) </param>
+        public void SetMemPwrAIndex(int value, UInt16 memoryNumber)
+        {
+            if (value != memPwrAIndex[--memoryNumber])
+            {
+                memPwrAIndex[memoryNumber] = value;
+
+                UpdateUiElements();
+            }
+
+        }
+
+        /// <summary>
+        /// This function set index of power at synthesizer output B for specific memory 
+        /// </summary>
+        /// <param name="value"> index of power 
+        /// ('0' for "-4dBm", 
+        ///  '1' for "-1dBm", 
+        ///  '2' for "+2dBm", 
+        ///  '3' for "!!!+5dBm!!!" [this value is disabled, because freq doubler allow max +3dBm at his input],
+        ///  otherwise "Disabled" )
+        /// </param>
+        /// <param name="memoryNumber"> specific memory number (1-4) </param>
+        public void SetMemPwrBIndex(int value, UInt16 memoryNumber)
+        {
+            if (value != memPwrBIndex[--memoryNumber])
+            {
+                memPwrBIndex[memoryNumber] = value;
+
+                UpdateUiElements();
+            }
+        }
+        
+        #endregion
     }
 }
