b07f5fd1c9b9d68774d668c3f13263ae271dfe23
Filip Klapil <klapi.filip@seznam.cz>
2020-04-01 15:49:50 +0200

    added code comment for ModuleControls

--- Synthesizer_PC_control/Model/ModuleControls.cs
+++ Synthesizer_PC_control/Model/ModuleControls.cs
@@ -3,16 +3,38 @@ using System.Windows.Forms;
 using Synthesizer_PC_control.Utilities;
 
 namespace Synthesizer_PC_control.Model
-{
+{   
+    /// <summary>
+    /// This class is used to handle the synthesizer module states (outputs, reference signal).
+    /// </summary>
     class ModuleControls : I_UiLinked
     {
+        /// <summary>
+        /// hold value if is active output 1
+        /// </summary>
         private bool out1State;
+
+        /// <summary>
+        /// hold value if is active output 2
+        /// </summary>
         private bool out2State;
+
+        /// <summary>
+        /// hold value if is active internal reference or external
+        /// </summary>
         private bool intRefState;
+
+        // hold synthesizer module UI elements
         private readonly Button ui_out1OnOff;
         private readonly Button ui_out2OnOff;
         private readonly Button ui_intExtRef;
         
+        /// <summary>
+        /// Constructor for ModuleControls
+        /// </summary>
+        /// <param name="ui_out1OnOff"> UI element for module output 1 button </param>
+        /// <param name="ui_out2OnOff"> UI element for module output 2 button </param>
+        /// <param name="ui_intExtRef"> UI element for module reference button </param>
         public ModuleControls(Button ui_out1OnOff, Button ui_out2OnOff, Button ui_intExtRef)
         {
             this.ui_out1OnOff = ui_out1OnOff;
@@ -26,6 +48,31 @@ namespace Synthesizer_PC_control.Model
             UpdateUiElements();
         }
 
+        public void UpdateUiElements() 
+        { 
+            if(out1State)    // Out 1 is active
+                ui_out1OnOff.Text = "Out 1 Off"; 
+            else
+                ui_out1OnOff.Text = "Out 1 On"; 
+            
+            if(out2State)    // Out 2 is active
+                ui_out2OnOff.Text = "Out 2 Off"; 
+            else
+                ui_out2OnOff.Text = "Out 2 On"; 
+
+            if(intRefState)    // Internal reference is active
+                ui_intExtRef.Text = "Ext Ref"; 
+            else
+                ui_intExtRef.Text = "Int Ref"; 
+        } 
+
+        #region Getters
+
+        /// <summary>
+        /// This function using for creating control register value.
+        /// From actual module controls states create hexademimal string value
+        /// </summary>
+        /// <returns> string represents hexedecimal value of control register </returns>
         public string GetControlRegister()
         {
             UInt32 control_register = 0;
@@ -48,6 +95,41 @@ namespace Synthesizer_PC_control.Model
             return Convert.ToString(control_register, 16);
         }
 
+        /// <summary>
+        /// This function get synthesizer module output 1 state
+        /// </summary>
+        /// <returns> true - active, false - disabled </returns>
+        public bool GetOut1State()
+        {
+            return out1State;
+        }
+
+        /// <summary>
+        /// This function get synthesizer module output 2 state
+        /// </summary>
+        /// <returns> true - active, false - disabled </returns>
+        public bool GetOut2State()
+        {
+            return out2State;
+        }
+
+        /// <summary>
+        /// This function get synthesizer module internal reference state
+        /// </summary>
+        /// <returns> true - internal, false - external </returns>
+        public bool GetIntRefState()
+        {
+            return intRefState;
+        }
+
+        #endregion
+
+        #region Setters
+        
+        /// <summary>
+        /// Use this function to set synth. output 1
+        /// </summary>
+        /// <param name="value"> true - enable, false - disable </param>
         public void SetOut1(bool value)
         {
             if (out1State != value)
@@ -58,6 +140,10 @@ namespace Synthesizer_PC_control.Model
             }
         }
 
+        /// <summary>
+        /// Use this function to set synth. output 2
+        /// </summary>
+        /// <param name="value"> true - enable, false - disable </param>
         public void SetOut2(bool value)
         {
             if (out2State != value)
@@ -68,6 +154,10 @@ namespace Synthesizer_PC_control.Model
             }
         }
 
+        /// <summary>
+        /// Use this function to set synth. signal reference
+        /// </summary>
+        /// <param name="value"> true - internal, false - external </param>
         public void SetIntRef(bool value)
         {
             if (intRefState != value)
@@ -78,37 +168,6 @@ namespace Synthesizer_PC_control.Model
             }
         }
 
-        public bool GetOut1State()
-        {
-            return out1State;
-        }
-
-        public bool GetOut2State()
-        {
-            return out2State;
-        }
-
-        public bool GetIntRefState()
-        {
-            return intRefState;
-        }
-
-        public void UpdateUiElements() 
-        { 
-            if(out1State)    // Out 1 je aktivni
-                ui_out1OnOff.Text = "Out 1 Off"; 
-            else
-                ui_out1OnOff.Text = "Out 1 On"; 
-            
-            if(out2State)    // Out 2 je aktivni
-                ui_out2OnOff.Text = "Out 2 Off"; 
-            else
-                ui_out2OnOff.Text = "Out 2 On"; 
-
-            if(intRefState)    // Interní reference je aktivni
-                ui_intExtRef.Text = "Ext Ref"; 
-            else
-                ui_intExtRef.Text = "Int Ref"; 
-        } 
+        #endregion
     }
 }
