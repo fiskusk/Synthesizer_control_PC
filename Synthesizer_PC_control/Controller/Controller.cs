fcf4d3fe5e4b810e12b28430dafbd415684d749c
Filip Klapil <klapi.filip@seznam.cz>
2020-04-04 18:34:54 +0200

    Added code comments for ReadRegister class

--- Synthesizer_PC_control/Controller/Controller.cs
+++ Synthesizer_PC_control/Controller/Controller.cs
@@ -1,13 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Drawing;
 using System.Windows.Forms;
 using Synthesizer_PC_control.Model;
 using Synthesizer_PC_control.Utilities;
-using System.Text.RegularExpressions;
 
 namespace Synthesizer_PC_control.Controllers
 {
@@ -158,6 +154,7 @@ namespace Synthesizer_PC_control.Controllers
                                           view.AutoCDIVCalcCheckBox, 
                                           view.DelayInputNumericUpDown);
 
+            // create model of PLO read register 6 
             readRegister = new ReadRegister(view.ReadedVCOValueTextBox,
                                             view.ReadedADCValueTextBox,
                                             view.ADCModeComboBox);
--- Synthesizer_PC_control/Model/ReadRegister.cs
+++ Synthesizer_PC_control/Model/ReadRegister.cs
@@ -2,22 +2,48 @@ using System.Windows.Forms;
 
 namespace Synthesizer_PC_control.Model
 {
+    /// <summary>
+    /// This class is used to handle the read register. 
+    /// (readed current VCO, readed ADC, adc mode)
+    /// </summary>
     public class ReadRegister
     {
+        /// <summary>
+        /// Enumerable for ADC mode (TEMPERATURE, TUNE_PIN)
+        /// </summary>
         public enum AdcMode
         {
             TEMPERATURE,
             TUNE_PIN
         }
 
+        /// <summary>
+        /// Current VCO number
+        /// </summary>
         private string readedCurrentVCO;
+
+        /// <summary>
+        /// readed ADC value
+        /// </summary>
         private string readedADC;
+
+        /// <summary>
+        /// ADC mode
+        /// </summary>
         private AdcMode adcMode;
 
+        // hold UI elements for read register group
         private readonly TextBox ui_ReadedCurrentVCO;
         private readonly TextBox ui_ReadedADC;
         private readonly ComboBox ui_AdcMode;
 
+        /// <summary>
+        /// Contructor for the read register. 
+        /// (readed current VCO, readed ADC, adc mode)
+        /// </summary>
+        /// <param name="ui_ReadedCurrentVCO"> TextBox UI element for current VCO number </param>
+        /// <param name="ui_ReadedADC"> TextBox UI element for readed ADC value </param>
+        /// <param name="ui_AdcMode"> TextBox UI element for ADC mode </param>
         public ReadRegister(TextBox ui_ReadedCurrentVCO, TextBox ui_ReadedADC,
                             ComboBox ui_AdcMode)
         {
@@ -26,6 +52,22 @@ namespace Synthesizer_PC_control.Model
             this.ui_AdcMode = ui_AdcMode;
         }
 
+        /// <summary>
+        /// Updates all graphic user elements
+        /// </summary>
+        public void UpdateUiElements()
+        {
+            ui_ReadedCurrentVCO.Text = readedCurrentVCO;
+            ui_ReadedADC.Text = readedADC;
+            ui_AdcMode.SelectedIndex = (int)adcMode;
+        }
+
+        #region Setters
+
+        /// <summary>
+        /// Sets current VCO number
+        /// </summary>
+        /// <param name="value"> VCO number as string </param>
         public void SetReadedCurrentVCO(string value)
         {
             readedCurrentVCO = value;
@@ -33,6 +75,10 @@ namespace Synthesizer_PC_control.Model
             UpdateUiElements();
         }
 
+        /// <summary>
+        /// Sets readed ADC value
+        /// </summary>
+        /// <param name="value"> adc value as string </param>
         public void SetReadededADC(string value)
         {
             readedADC = value;
@@ -40,6 +86,10 @@ namespace Synthesizer_PC_control.Model
             UpdateUiElements();
         }
 
+        /// <summary>
+        /// Sets ADC mode
+        /// </summary>
+        /// <param name="value"> adc mode </param>
         public void SetAdcMode(AdcMode value)
         {
             adcMode = value;
@@ -47,16 +97,20 @@ namespace Synthesizer_PC_control.Model
             UpdateUiElements();
         }
 
+        #endregion
+
+        #region Getters
+
+        /// <summary>
+        /// This funtion gets ADC mode
+        /// </summary>
+        /// <returns> adc mode </returns>
         public AdcMode GetAdcMode()
         {
             return adcMode;
         }
 
-        public void UpdateUiElements()
-        {
-            ui_ReadedCurrentVCO.Text = readedCurrentVCO;
-            ui_ReadedADC.Text = readedADC;
-            ui_AdcMode.SelectedIndex = (int)adcMode;
-        }
+        #endregion
+
     }
 }
