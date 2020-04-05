df551b0dc339936c5e33ca8fb520133948c7a31d
Filip Klapil <klapi.filip@seznam.cz>
2020-04-05 16:24:41 +0200

    CalcRegisters code commented and refactored

--- Synthesizer_PC_control/Utilities/CalcRegisters.cs
+++ Synthesizer_PC_control/Utilities/CalcRegisters.cs
@@ -7,26 +7,80 @@ using System.Linq;
 namespace Synthesizer_PC_control.Utilities
 {
     /// <summary>
-    /// struct for calculated register settings from input frequency
+    /// Structure for calculated register settings from input frequency
     /// </summary>
     public struct CalcRegs
     {
+        /// <summary>
+        /// Integer-N value
+        /// </summary>
         public UInt16 intN;
+
+        /// <summary>
+        /// Fractional-N value
+        /// </summary>
         public UInt16 fracN;
+
+        /// <summary>
+        /// Modulus value
+        /// </summary>
         public UInt16 mod;
+
+        /// <summary>
+        /// R-divider value
+        /// </summary>
         public UInt16 rDiv;
+
+        /// <summary>
+        /// PLO mode
+        /// </summary>
         public SynthMode mode;
+
+        /// <summary>
+        /// A-divider index value
+        /// </summary>
         public int aDivIndex;
+
+        /// <summary>
+        /// calculated frequency
+        /// </summary>
         public decimal calcFreq;
+
+        /// <summary>
+        /// desired frequency
+        /// </summary>
         public decimal desiredFreq;
     }
     
+    /// <summary>
+    /// Registers values calculations. It contains public function for calculating 
+    /// registers values from input desired frequency.
+    /// </summary>
     public static class  CalcRegisters
     {
+        /// <summary>
+        /// It holds reference input frequency
+        /// </summary>
         private static decimal static_refInFreq;
+
+        /// <summary>
+        /// It holds if reference frequency is doubled
+        /// </summary>
         private static bool static_isDoubled;
+
+        /// <summary>
+        /// It holds if reference frequency is divided by two
+        /// </summary>
         private static bool static_isDivBy2;
+
+        /// <summary>
+        /// It holds RF path output B
+        /// </summary>
         private static int static_outBPathIndex;
+
+        /// <summary>
+        /// It holds VCO to integer-counter feedback path
+        /// </summary>
         private static int static_FBPathIndex;
         
         /// <summary>
@@ -53,30 +107,44 @@ namespace Synthesizer_PC_control.Utilities
             static_outBPathIndex  = outBPathIndex;
             static_FBPathIndex    = FBPathIndex;            // FIXME implement FBPath into calculations
 
-            CalcRegs calcRegs = new CalcRegs();     // variable for calculated reg values
-            CalcRegs[] calculatedSettings = new CalcRegs[100];  // variable for store each calc register
+            /// <summary>
+            ///  variable for store each calc register  
+            /// </summary>
+            CalcRegs[] calculatedSettings = new CalcRegs[100];  
             
-            // calc in parralel task 50 down
+            /// <summary>
+            /// calc in parralel task 50x by 1Hz decrement, including f_input value
+            /// </summary>
             Action calcJob1 = () => 
             {
+                // initial value. In first step include f_input value
                 decimal copyFInputDown = f_input + 0.000001M;
                 ThreadPool.SetMinThreads(50,50);
+
                 ParallelEnumerable.Range(0, 50).WithDegreeOfParallelism(50).ForAll(i =>
                 {
+                    // in each step decrese frequency by 1Hz
                     copyFInputDown = copyFInputDown - 0.000001M;
-                    calculatedSettings[i] = CalcFracModVals(copyFInputDown);
+                    // and recalculate new settings and store it into array
+                    calculatedSettings[i] = CalcRegSettings(copyFInputDown);
                 });
             };
 
-            // calc in parralel task 50 up
+            /// <summary>
+            /// calc in parralel task 50x by 1Hz increment
+            /// </summary>
             Action calcJob2 = () => 
             {
+                // initial value.
                 decimal copyFInputUp = f_input;
-                ThreadPool.SetMinThreads(50,50);
+                ThreadPool.SetMinThreads(50,50);    
+
                 ParallelEnumerable.Range(50, 50).WithDegreeOfParallelism(50).ForAll(i =>
                 {
+                    // in each step increase frequency by 1Hz
                     copyFInputUp = copyFInputUp + 0.000001M;
-                    calculatedSettings[i] = CalcFracModVals(copyFInputUp);
+                    // and recalculate new settings and store it into array
+                    calculatedSettings[i] = CalcRegSettings(copyFInputUp);
                 });
             };
 
@@ -90,7 +158,22 @@ namespace Synthesizer_PC_control.Utilities
             Task.WaitAll(tasks);
 
 
-            // now find mimimum delta a store this corrensponding values
+            // now find mimimum delta and return corrensponding values
+            int indexOfMinimum = 0;
+            indexOfMinimum = GetIndexForMinDelta(f_input, calculatedSettings);
+
+            return calculatedSettings[indexOfMinimum];
+        }
+
+        /// <summary>
+        /// This function find position of smallest delta between 
+        /// calculated frequency and desired frequency in calculated settings array
+        /// </summary>
+        /// <param name="f_input"> desired frequency </param>
+        /// <param name="calculatedSettings"> data array of calculated settings </param>
+        /// <returns></returns>
+        private static int GetIndexForMinDelta(decimal f_input, CalcRegs[] calculatedSettings)
+        {
             int indexOfMinimum = 0;
             decimal deltaMinimum = Math.Abs((f_input - calculatedSettings[indexOfMinimum].calcFreq) * 1e6M);
             decimal delta;
@@ -106,20 +189,18 @@ namespace Synthesizer_PC_control.Utilities
                 }
             }
 
-            calcRegs = calculatedSettings[indexOfMinimum];
-
-            return calcRegs;
+            return indexOfMinimum;
         }
 
         /// <summary>
-        /// This function try to find optimal register values
+        /// This function try to find optimal register values for desired values
         /// </summary>
         /// <param name="desiredFreq"> frequency from which the register values are to be calculated </param>
         /// <returns> Calculated register values </returns>
-        public static  CalcRegs CalcFracModVals(decimal desiredFreq)
+        private static  CalcRegs CalcRegSettings(decimal desiredFreq)
         {
             // declare variables for calculated values
-            UInt16      rDivValue = 1; // preset R-divider value to one
+            UInt16      rDivValue = 1;  // initial R-divider value 
             UInt16      aDivIndex;
             UInt16      intN;
             SynthMode   mode;
@@ -129,17 +210,21 @@ namespace Synthesizer_PC_control.Utilities
             decimal     remainder;
             decimal     calcFreq;
 
+            /// <summary>
+            /// calculated register settings
+            /// </summary>
             CalcRegs calcRegs = new CalcRegs();
 
-            // first calculate integer part and get remainder
+            // first calculate integer part and eventualy remainder
             CalcIntNFromFrequency(desiredFreq, rDivValue, 
                                 out intN, out remainder, 
                                 out aDivIndex, out fPfd);
 
-            // if remainder is greater than approx. zero
             if (remainder > 0.0000000001M)
             {
+                // if remainder is greater than approx. zero, then calculate fractional part
                 mode = SynthMode.FRACTIONAL;    // set mode as Fractional
+
                 bool success;
                 success = TryToCalcFractPart(remainder, rDivValue, 
                                         out rDivValue, intN, 
@@ -147,7 +232,7 @@ namespace Synthesizer_PC_control.Utilities
                                         out mod, desiredFreq,
                                         aDivIndex, fPfd, out fPfd);
 
-                // if success calc frequency from this values
+                // if success calculate according frequency from this values
                 if (success == true)
                 {
                     calcFreq = CalcFreqInfo(mode, intN,
@@ -157,19 +242,22 @@ namespace Synthesizer_PC_control.Utilities
                 }
                 else
                 {
-                    calcFreq = 0;
+                    calcFreq = 0;   // if the calculations was not successful, set zero 
                 }
             }
             else
             {
+                // if remainder is zero, this is Integer mode
                 mode = SynthMode.INTEGER;   // synth. mode is integer
-                fracN = 0;
-                mod = 0;
+                fracN = 0;  // it doesn't matter
+                mod = 0;    // it doesn't matter
+                // calculate according frequency from this values
                 calcFreq = CalcFreqInfo(mode, intN,
                                         fracN, mod, desiredFreq, 
                                         fPfd, aDivIndex);
             }
 
+            // return calculated register settings
             calcRegs.intN = intN;
             calcRegs.fracN = fracN;
             calcRegs.mod = mod;
@@ -191,25 +279,31 @@ namespace Synthesizer_PC_control.Utilities
         /// <param name="remainder"> the remainder of the division </param>
         /// <param name="aDivIndex"> calculated corresponding A-divider Index </param>
         /// <param name="fPfd"> calculated frequency at PFD input </param>
-        public static void CalcIntNFromFrequency(decimal frequency, UInt16 rDivValue,
+        private static void CalcIntNFromFrequency(decimal frequency, UInt16 rDivValue,
                                           out UInt16 intN, out decimal remainder,
                                           out UInt16 aDivIndex, out decimal fPfd)
         {
-            // freq at PFD input
+            // calculate frequency at PFD input
             fPfd = static_refInFreq * ((1 + Convert.ToUInt16(static_isDoubled)) / 
                 (decimal)(rDivValue * (1 + Convert.ToUInt16(static_isDivBy2))));
 
+            // calculate Int-N value for corresponding band
             decimal intN_decimal;
 
-            // calc Int-N value for corresponding band
             if (frequency <= 6000)
             {
-                aDivIndex = FindAndSetCorrespondingADivValue(frequency);    // find corresponding A-Div index
-                intN_decimal = (frequency * (1 << aDivIndex) / fPfd);       // calculate Int-N as decimal number
+                // find corresponding A-Div index
+                aDivIndex = FindADivIndex(frequency);
+                // calculate Int-N as decimal number
+                intN_decimal = (frequency * (1 << aDivIndex) / fPfd);
             }
             else
             {
-                aDivIndex = FindAndSetCorrespondingADivValue(frequency / 2);
+                // find corresponding A-Div index
+                aDivIndex = FindADivIndex(frequency / 2);
+
+                // calculate Int-N as decimal number with respect to the set 
+                // RF output B path
                 if (static_outBPathIndex == 0)
                 {
                     intN_decimal = (frequency / 2) * (1 << aDivIndex) /  fPfd;
@@ -230,7 +324,7 @@ namespace Synthesizer_PC_control.Utilities
         /// </summary>
         /// <param name="value"> frequency to which the A-div value is sought </param>
         /// <returns> finded A-divider index value </returns>
-        private static UInt16 FindAndSetCorrespondingADivValue(decimal value)
+        private static UInt16 FindADivIndex(decimal value)
         {
             UInt16 divAIndex;
             if (value >= 3000 && value <= 6000)
@@ -295,41 +389,53 @@ namespace Synthesizer_PC_control.Utilities
                                        decimal frequency, UInt16 aDivIndex,
                                        decimal fPfdIn, out decimal fPfd)
         {
+            /// <summary>
+            /// success of operation
+            /// </summary>
             bool success;
-            fPfd = fPfdIn;
 
-            rDivValue = rDivValueIn;
-            intN = (UInt16)intNIn;
+            fPfd = fPfdIn;              // load initial PFD frequency
+            rDivValue = rDivValueIn;    // load initial R-divider value
+            intN = intNIn;              // load initial Integer-N value
 
             Fractions.Fraction fracPart = new Fractions.Fraction();
+            
+            double accuracy;            // accuracy of coversion
+            int correction=1000;        
 
-            double accuracy;
-            int correction=1000;
-            UInt16 cnt = 0;
-
+            // do this while applies that denominator is within limits (2-4095 including)
+            // and  accuracy is less than 1.
             do
             {
                 accuracy = 0.000001;
+                // do this while applies that denominator is within limits (2-4095 including)
+                // and  accuracy is less than 0.00001 * correction.
                 do
                 {
                     if (remainder > 0.0000000001M)
                         fracPart = Fractions.RealToFraction((double)remainder, accuracy);
-                    cnt++;
+
                     accuracy = accuracy*10;
                 } while ((fracPart.D < 2 || fracPart.D > 4095) && accuracy <= 0.00001*correction);
+
+                // check that the denominator is not outside the limits
                 if ((fracPart.D < 2 || fracPart.D > 4095))
                 {
+                    // if yes increase R-divider value by one
                     rDivValue++;
+                    // and then re-calculate Integer-N value
                     CalcIntNFromFrequency(frequency, rDivValue,  out intN, 
                                           out remainder, out aDivIndex, out fPfd);
+                    // if new calculated Int-N value is still to high
                     if (intN > 4091)
                     {
+                        // accuracy set to one, do-while loop will be terminated
                         accuracy = 1;
                         success = false;
                     }
                 }
             } while((fracPart.D < 2 || fracPart.D > 4095) && accuracy < 1);
-
+            
             if ((fracPart.D < 2 || fracPart.D > 4095))
             {
                 success = false;
@@ -340,12 +446,23 @@ namespace Synthesizer_PC_control.Utilities
                 success = true;
             }
 
-            mod = (UInt16)fracPart.D;
-            fracN = (UInt16)fracPart.N;
+            mod = (UInt16)fracPart.D;       // get modulus value
+            fracN = (UInt16)fracPart.N;     // get Fractional-N value
 
             return success;
         }
 
+        /// <summary>
+        /// This function calculate frequency info from register settings
+        /// </summary>
+        /// <param name="mode"> PLO mode (FRACTIONAL, INTEGER) </param>
+        /// <param name="intN"> Integer-N value </param>
+        /// <param name="fracN"> Fractional-N value </param>
+        /// <param name="mod"> modulus value </param>
+        /// <param name="desiredFreq"> desired output frequency </param>
+        /// <param name="fPfd"> frequency at PFD input </param>
+        /// <param name="aDivIndex"> A-divider index number </param>
+        /// <returns> output calculated frequency </returns>
         private static decimal CalcFreqInfo(SynthMode mode, UInt16 intN, 
                                     UInt16 fracN, UInt16 mod, 
                                     decimal desiredFreq, decimal fPfd, UInt16 aDivIndex)
@@ -371,7 +488,6 @@ namespace Synthesizer_PC_control.Utilities
             }
 
             return calcFreq;
-
         }
     }
 }
